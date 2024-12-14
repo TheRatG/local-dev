ef8b8e1d4198a72b6ba2ee8aa5fe941ce88860fb
Vladimir Pak <theratg@gmail.com>
2024-12-14 14:39:48 +0200

    mysql cnf

---
+++ 30_App/mysql/dump.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+DATABASE='<database>'
+NAME=dump
+FILENAME="~/${PROJECT_NAME}.sql.gz"
+
+COMMON_ARGS=(
+  '--column-statistics=0'
+  '--single-transaction'
+  '--skip-lock-tables'
+  '--no-tablespaces'
+  '--skip-triggers'
+  '--set-gtid-purged=OFF'
+);
+COMMON_ARGS=$( IFS=$' '; echo "${COMMON_ARGS[*]}" );
+
+IGNORE_TABLES=(
+  "--ignore-table=${DATABASE}.logs"
+);
+IGNORE_TABLES=$( IFS=$' '; echo "${IGNORE_TABLES[*]}" )
+
+mysqldump --no-data $COMMON_ARGS $DATABASE | gzip > $FILENAME
+mysqldump --no-create-info -Q -c -e --hex-blob $COMMON_ARGS $IGNORE_TABLES $DATABASE | gzip >> $FILENAME
+
+echo "Dump file created: ${FILENAME}";
